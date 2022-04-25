6f463a1fee8d334b0e197d2436f735d1e2f5cc39
Anibrata Malkhandi <99401003+anibrata53@users.noreply.github.com>
2022-04-25 17:30:14 +0530

    Add files via upload

---
+++ crud/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"crud/database"
+	_ "github.com/jinzhu/gorm/dialects/mysql" //Required for MySQL dialect
+)
+
+func main() {
+	config :=
+		database.Config{
+			ServerName: "localhost:3306",
+			User:       "root",
+			Password:   "root",
+			DB:         "learning",
+		}
+
+	connectionString := database.GetConnectionString(config)
+	err := database.Connect(connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+}
